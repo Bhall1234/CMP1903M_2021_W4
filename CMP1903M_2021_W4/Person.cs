7abd8e0527aaf96cd872cf11eb74179210568dee
Mark Doughty <mdoughty@lincoln.ac.uk>
2021-03-08 14:11:35 +0000

    Add project files.

---
+++ CMP1903M_2021_W4/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMP1903M_2021_W4
+{
+    class Person
+    {
+        //fields
+        private string _firstName;
+        private string _secondName;
+        private string _email;
+
+        //properties
+      
+
+        //constructors
+      
+
+        //methods
+ 
+    
+    }
+}
---
+++ CMP1903M_2021_W4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMP1903M_2021_W4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Space to test your Person class, the properties, constructors and methods used.
+            
+        }
+    }
+}
